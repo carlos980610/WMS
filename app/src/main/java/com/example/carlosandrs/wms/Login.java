afa83dadf35a9bb08c37f8d0f95271e51276cfea
Diego <diego.correa33@hotmail.es>
2017-01-03 20:45:15 -0500

    hy

--- app/src/main/java/com/example/carlosandrs/wms/Login.java
+++ app/src/main/java/com/example/carlosandrs/wms/Login.java
@@ -12,8 +12,8 @@ import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
-    //yhyh
-    //jsjs
+    //hy
+    //sj
     private String user, password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {

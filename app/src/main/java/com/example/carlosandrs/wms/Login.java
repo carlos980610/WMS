671e7d85a60ce2e767543691d4ec4bed20e2fe9c
Carlos Andrés Montoya Cardona <capianomusical@hotmail.com>
2017-01-04 00:44:45 +0000

    Updated Login.java

--- app/src/main/java/com/example/carlosandrs/wms/Login.java
+++ app/src/main/java/com/example/carlosandrs/wms/Login.java
@@ -12,7 +12,7 @@ import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
     
-    //jsjs
+    //jsjs  gfgfgfg
     private String user, password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
